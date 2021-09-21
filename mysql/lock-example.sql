5ec9af2717d93fe32762ad7b4663e5c04dbb35c3
Takuya Murakami <tmurakam@tmurakam.org>
2021-09-21 10:19:05 +0900

    add sql

---
+++ mysql/lock-example.sql
@@ -0,0 +1,17 @@
+-- Get X lock
+BEGIN;
+
+SELECT * from test WHERE id = 1 FOR UPDATE;
+
+
+-- Get S lock
+BEGIN;
+
+SELECT * from test WHERE id = 1 LOCK IN SHARE MODE;
+
+
+-- Create temp table (get S lock)
+BEGIN;
+
+DROP TEMPORARY TABLE IF EXISTS temp;
+CREATE TEMPORARY TABLE temp SELECT * from test WHERE id = 1;
